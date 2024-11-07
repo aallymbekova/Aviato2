66498d1ff63db63471e26105610baae3f7d30061
aallymbekova <alymbekovaaliya@icloud.com>
2024-11-07 23:34:13 +0600

    Initial Commit

---
+++ Aviator2/Aviator2App.swift
@@ -0,0 +1,17 @@
+//
+//  Aviator2App.swift
+//  Aviator2
+//
+//  Created by Aliya Alymbekova on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Aviator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aviator2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aviator2
+//
+//  Created by Aliya Alymbekova on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
